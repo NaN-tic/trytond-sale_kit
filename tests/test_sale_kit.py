0df1e7c4e51714a9a47af2dcc2fefd0b7bb25a0d
Sergi Almacellas Abellana <sergi@koolpi.com>
2014-11-04 16:07:55 +0100

    Migrate to version 3.4

--- tests/test_sale_kit.py
+++ tests/test_sale_kit.py
@@ -1,39 +1,24 @@
 #!/usr/bin/env python
-#This file is part of sale_kit module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains
-#the full copyright notices and license terms.
-
-import os
-import sys
+# This file is part of sale_kit module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 import unittest
-
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
-
 from trytond.tests.test_tryton import test_view, test_depends
 import trytond.tests.test_tryton
 
 
 class SaleKitTestCase(unittest.TestCase):
-    '''
-    Test Sale Kit module.
-    '''
+    'Test Sale Kit module'
 
     def setUp(self):
         trytond.tests.test_tryton.install_module('sale_kit')
 
     def test0005views(self):
-        '''
-        Test views.
-        '''
+        'Test views'
         test_view('sale_kit')
 
     def test0006depends(self):
-        '''
-        Test depends.
-        '''
+        'Test depends'
         test_depends()
 
 
@@ -42,6 +27,3 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         SaleKitTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
