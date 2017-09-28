0a91d1e09002994201c0eafa75d4b1f188301ddd
Raimon Esteve <raimon@nan-tic.com>
2017-09-28 17:59:28 +0200

    Add scenario sale kit test

--- tests/test_sale_kit.py
+++ tests/test_sale_kit.py
@@ -2,8 +2,11 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 import unittest
+import doctest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import ModuleTestCase
+from trytond.tests.test_tryton import (doctest_setup, doctest_teardown,
+    doctest_checker)
 
 
 class SaleKitTestCase(ModuleTestCase):
@@ -15,4 +18,8 @@ def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         SaleKitTestCase))
+    suite.addTests(doctest.DocFileSuite('scenario_sale_kit.rst',
+            setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
+            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE,
+            checker=doctest_checker))
     return suite
